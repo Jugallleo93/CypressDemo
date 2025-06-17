2af3626a0f9fff5a6fc22a0a7ef5a3a1307236e6
jugal.panchal <jugal.panchal@accenture.com>
2025-06-17 13:14:36 +0530

    my second commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,36 @@
 const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib')
 
 module.exports = defineConfig({
+
+  reporter: 'cypress-mochawesome-reporter',
+  video: true,
+  screenshotOnRunFailure: true,
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress Execution Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+
   e2e: {
+    baseUrl: 'https://naveenautomationlabs.com/opencart/index.php?route=account/login',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:run', async (details) => {
+        console.log('override before:run')
+        await beforeRunHook(details)
+      })
+
+      on('after:run', async () => {
+        console.log('override after:run')
+        await afterRunHook()
+      })
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
 
-    env:{
-      URL:'https://naveenautomationlabs.com/opencart/index.php?route=account/register'
+    env: {
+      URL: 'https://naveenautomationlabs.com/opencart/index.php?route=account/register'
       //URL:'https://gcphub.ciostage.accenture.com'
     }
   },
